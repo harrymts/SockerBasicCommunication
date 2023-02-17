5422c9835899d3a91d21e9551868849ca63cfc53
Harry Mitsis <87229378+harrymts@users.noreply.github.com>
2023-02-17 15:43:44 +0100

    Delete redminepython.py

--- redminepython.py
+++
@@ -1,7 +0,0 @@
-from redminelib import Redmine
-
-redmine = Redmine(settings.REDMINE_URL, key=settings.REDMINE_KEY)
-#redmine = Redmine('http://172.28.22.142:3000', key='43fee4944a913adbfd27c000f28d90d23514c172', verify=False)
-project = redmine.project.get('testing-the-functionalities')
-project.identifier
-project.created_on
